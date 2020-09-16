df0b61ccb0796b47884688671b8e50d60289d0a8
ecastan960 <ecastan1@eafit.edu.co>
2020-09-16 21:43:28 +0000

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/*
+*This program prints a sentence
+*/
+#include<stdio.h>
+/**
+ * main - This is a description
+ * Return: The sum of the two parameters
+ */
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
