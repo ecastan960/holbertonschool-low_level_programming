abc36c6a2f6bef3f2a73cd10c2b972f91e7c3419
ecastan960 <ecastan1@eafit.edu.co>
2020-09-16 20:44:47 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 char str1[60];
-strcpy(str1, "\"Programming is like building a multilingual puzzle");
+strcpy(str1, "\"Programming is like building a multilingual puzzle\n");
 puts(str1);
 return (0);
 }
