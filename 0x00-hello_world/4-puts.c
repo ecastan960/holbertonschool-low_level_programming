071880fae22cca7b9d71e355c5c28463c5de4242
ecastan960 <ecastan1@eafit.edu.co>
2020-09-16 21:19:50 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,14 @@
 */
 #include<stdio.h>
 #include <string.h>
+/**
+ * main - This is a description
+ * return: 0 characters
+ */
 int main(void)
 {
 char str1[60];
-strcpy(str1, "\"Programming is like building a multilingual puzzle\n");
+strcpy(str1, "\"Programming is like building a multilingual puzzle");
 puts(str1);
 return (0);
 }
