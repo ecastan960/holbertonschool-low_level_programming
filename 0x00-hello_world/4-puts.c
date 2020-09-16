aa0f50f8d62aca248898d4fe11091777fc853a3e
ecastan960 <ecastan1@eafit.edu.co>
2020-09-16 20:43:49 +0000

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/*
+*This program prints a sentence
+*/
+#include<stdio.h>
+#include <string.h>
+int main(void)
+{
+char str1[60];
+strcpy(str1, "\"Programming is like building a multilingual puzzle");
+puts(str1);
+return (0);
+}
