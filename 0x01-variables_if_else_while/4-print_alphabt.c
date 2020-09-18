0a503a3f9db7cbb539acc499673c761fe2f0d1d6
ecastan960 <ecastan1@eafit.edu.co>
2020-09-18 01:55:34 +0000

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+/**
+ * main - This program will assign a random number
+ * if the number is positive, negative or zero.
+ *
+ * Description: prints numbers
+ * and the prints if it is positive,negative or zero
+ * Return: 0
+ *
+ */
+#include <stdio.h>
+int main(void)
+{
+int l;
+for (l = 'a' ; l <= 'z' ; l++)
+{
+if (l == 'q' || l == 'e')
+{
+}
+else
+{
+putchar(l);
+}
+}
+putchar('\n');
+return (0);
+}
