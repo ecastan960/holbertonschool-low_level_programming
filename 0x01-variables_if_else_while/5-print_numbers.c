4403479f5a7428d8e50725fe2885adbb26dde98c
ecastan960 <ecastan1@eafit.edu.co>
2020-09-18 02:07:53 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+/**
+ * main - This program will assign a random number
+ * if the number is positive, negative or zero.
+ *
+ * Description: prints numbers
+ * and the prints if it is positive,negative or zero
+ * Return: 0
+ *
+ */
+#include <stdio.h>
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+printf("%d", x);
+}
+putchar('\n');
+return (0);
+}
