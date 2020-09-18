f393d7122df15064beefba74dfc3634498b52b0e
ecastan960 <ecastan1@eafit.edu.co>
2020-09-18 02:53:40 +0000

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+/**
+ * main - This program will assign a random number
+ * if the number is positive, negative or zero.
+ *
+ * Description: prints numbers
+ * and the prints if it is positive,negative or zero
+ * Return: 0
+ *
+ */
+#include <stdio.h>
+int main(void)
+{
+char l;
+for (l = 'z' ; l >= 'a' ; l--)
+{
+putchar(l);
+}
+putchar('\n');
+return (0);
+}
