3398bce4342b9577885c1c5b0849233d71f07b0b
ecastan960 <ecastan1@eafit.edu.co>
2020-09-22 23:45:19 +0000

    X10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_alphabet - This program will print using _putchar function
+ *
+ * Description: prints Holberton using the function
+ * Return: 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+int c;
+int b;
+for (b = 0; b < 10 ; b++)
+{
+for (c = 'a'; c <= 'z' ; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
