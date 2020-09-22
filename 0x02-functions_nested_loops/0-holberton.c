49ae12d224a08ac22e3cc33bd8987302f269486e
ecastan960 <ecastan1@eafit.edu.co>
2020-09-22 12:51:04 +0000

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,11 @@
 /**
  * main - This program will print using _putchar function
+ *
  * @c: The character to print
  * Description: prints Holberton using the function
- * _putchar()
  * Return: 0
  *
  */
-#include <holberton.h>
 int _putchar(char c);
 char h[] = "Holberton";
 int main(void)
