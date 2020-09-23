f136293f0780fe62f4c99eb52fb3256ff3af6522
ecastan960 <ecastan1@eafit.edu.co>
2020-09-23 19:53:19 +0000

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,19 +9,16 @@
  */
 void jack_bauer(void)
 {
-int a;
-int b;
-int c;
-int d;
-for (a = 0; a < 3; a++)
+int a, b, c, d;
+for ( a = 0; a < 3; a++)
 {
-if (a <2)
+if (a < 2)
 {
-for (b = 0; b < 10; b++)
+for ( b = 0; b < 10; b++)
 {
-for (c = 0; c < 6; c++)
+for ( c = 0; c < 6; c++)
 {
-for (d = 0; d < 10; d++)
+for ( d = 0; d < 10; d++)
 {
 _putchar(a + 48);
 _putchar(b + 48);
@@ -51,6 +48,5 @@ _putchar(10);
 }
 }
 }
-
 }
 }
