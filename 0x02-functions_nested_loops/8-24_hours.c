a8c9717bae87e663ef1aecfbacc9a4a8605eb199
ecastan960 <ecastan1@eafit.edu.co>
2020-09-23 19:44:00 +0000

    24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - This program will print using _putchar function
+ *
+ * Description: prints Holberton using the function
+ * Return: 0
+ *
+ */
+void jack_bauer(void)
+{
+int a;
+int b;
+int c;
+int d;
+for (a = 0; a < 3; a++)
+{
+for (b = 0; b < 4; b++)
+{
+for (c = 0; c < 6; c++)
+{
+for (d = 0; d < 10; d++)
+{
+_putchar(a+48);
+_putchar(b+48);
+_putchar(':');
+_putchar(c+48);
+_putchar(d+48);
+_putchar(10);
+}
+}
+}
+}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -8,3 +8,4 @@ int _abs(int);
 int print_last_digit(int n);
 int add(int a, int b);
 void times_table(void);
+void jack_bauer(void);
