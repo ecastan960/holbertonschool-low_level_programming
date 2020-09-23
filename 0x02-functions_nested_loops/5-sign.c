e50cddd710cc55f2385a307f86fb08118360558d
ecastan960 <ecastan1@eafit.edu.co>
2020-09-23 03:37:09 +0000

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * print_sign - This program will print using _putchar function
- * @c: The character to evaluate
+ * @n: The character to evaluate
  *
  * Description: prints Holberton using the function
  * Return: 0
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 if (n < 0)
 {
-_putchar('-'); 
+_putchar('-');
 return (-1);
 }
 else if (n > 0)
 {
-_putchar('+');  
+_putchar('+');
 return (1);
 }
 else
 {
-_putchar('0');  
+_putchar('0');
 return (0);
 }
 }
