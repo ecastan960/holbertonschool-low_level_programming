2158eb1268b2678a4756613398d9c09446ec73ad
ecastan960 <ecastan1@eafit.edu.co>
2020-09-23 16:43:35 +0000

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_last_digit - This program will print using _putchar function
+ * @n: The character to evaluate
+ *
+ * Description: prints Holberton using the function
+ * Return: 0
+ *
+ */
+int print_last_digit(int n)
+{
+int a;
+int b;
+if (n < 0)
+{
+n = n * -1;
+a = n % 10;
+b = a + 48;
+_putchar(b);
+return (a);
+}
+else if (n > 0)
+{
+a = n % 10;
+b = a + 48;
+_putchar(b);
+return (a);
+}
+else if (n == 0)
+{
+a = n % 10;
+b = a + 48;
+_putchar(b);
+return (a);
+}
+return (0);
+}
