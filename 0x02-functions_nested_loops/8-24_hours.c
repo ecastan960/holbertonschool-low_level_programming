60cd27725b7c13bb9fe028418056af0328f0aff6
ecastan960 <ecastan1@eafit.edu.co>
2020-09-23 19:50:03 +0000

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,6 +15,26 @@ int c;
 int d;
 for (a = 0; a < 3; a++)
 {
+if (a <2)
+{
+for (b = 0; b < 10; b++)
+{
+for (c = 0; c < 6; c++)
+{
+for (d = 0; d < 10; d++)
+{
+_putchar(a + 48);
+_putchar(b + 48);
+_putchar(':');
+_putchar(c + 48);
+_putchar(d + 48);
+_putchar(10);
+}
+}
+}
+}
+else
+{
 for (b = 0; b < 4; b++)
 {
 for (c = 0; c < 6; c++)
@@ -30,5 +50,7 @@ _putchar(10);
 }
 }
 }
+}
+
 }
 }
