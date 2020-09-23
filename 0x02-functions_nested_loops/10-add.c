0da67d982f91789b56abac525d1ee4db976cad89
ecastan960 <ecastan1@eafit.edu.co>
2020-09-23 04:46:21 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * add - This program will print using _putchar function
+ * @a: The character to evaluate
+ * @b: The character to evaluate
+ *
+ * Description: prints Holberton using the function
+ * Return: 0
+ *
+ */
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
