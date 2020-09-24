05ea5af34b1dcdbced9b1bb122164e9012468111
ecastan960 <ecastan1@eafit.edu.co>
2020-09-24 15:29:27 +0000

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - This program will print using _putchar function
+ * @a: input integer
+ * @b: input integer
+ *
+ * Description: prints Holberton using the function
+ * Return: 0
+ *
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
