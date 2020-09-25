8fb5a74a80ee56064890b2a66b066267e0ce1c21
ecastan960 <ecastan1@eafit.edu.co>
2020-09-25 01:25:15 +0000

    diag

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_line - This program will print using _putchar function
+ * @n: integer to evaluate
+ *
+ * Description: prints Holberton using the function
+ * Return: 0
+ *
+ */
+void print_diagonal(int n)
+{
+int a, b;
+if (n <= 0)
+{
+}
+else
+{
+if ( n > 1)
+{
+for (a = 0; a < n ; a++ )
+{
+for (b = 0 ; b < a ; b++ )
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
+_putchar('\n');
+}
