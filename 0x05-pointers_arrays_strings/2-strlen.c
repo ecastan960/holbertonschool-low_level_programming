e446b775d2145c22f54ec27d9f9f2507c5ed00a2
ecastan960 <ecastan1@eafit.edu.co>
2020-09-29 23:11:11 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - This program will print using _putchar function
+ * @s: string
+ * Description: prints Holberton using the function
+ * Return: 0
+ *
+ */
+
+int _strlen(char *s)
+{
+int a;
+for (a = 0; s[a] != '\0'; ++a)
+{
+}
+return (a);
+}
