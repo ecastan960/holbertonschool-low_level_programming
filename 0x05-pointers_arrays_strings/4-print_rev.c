64c1ea0d63c7e8056a027f239e9267aaa80331a1
ecastan960 <ecastan1@eafit.edu.co>
2020-09-30 11:55:24 +0000

    revp

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,10 +12,10 @@ void print_rev(char *s)
 {
 int a, b;
 
-for (a = 0; s[a] != '\0'; ++a)
+for (a = 1; s[a] != '\0'; ++a)
 {
 }
-for (b = a; b > 0; --b)
+for (b = a; b >= 0; --b)
 {
 _putchar(s[b]);
 }
