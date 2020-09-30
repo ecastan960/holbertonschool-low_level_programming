991eacf03c355391af5638755645061f0007e5dd
ecastan960 <ecastan1@eafit.edu.co>
2020-09-30 00:15:09 +0000

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_rev - This program will print using _putchar function
+ * @s: string
+ * Description: prints Holberton using the function
+ * Return: 0
+ *
+ */
+
+void print_rev(char *s)
+{
+int a, b;
+
+for (a = 0; s[a] != '\0'; ++a)
+{
+}
+for (b = a; b > 0; --b)
+{
+_putchar(s[b]);
+
+}
+_putchar('\n');
+}
