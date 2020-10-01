46619285506054fa270d74d624efe6425a527328
ecastan960 <ecastan1@eafit.edu.co>
2020-10-01 01:59:28 +0000

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * puts_half - This program will print using _putchar function
+ * @str: string
+ * Description: prints Holberton using the function
+ * Return: 0
+ *
+ */
+
+void puts_half(char *str)
+{
+int a, b, n;
+char *s1, c;
+s1 = str;
+for (a = 0; str[a] != '\0'; ++a)
+{
+}
+if (a % 2 == 0)
+{
+n = a / 2;
+}
+else
+{
+n = (a - 1) / 2;
+}
+for (b = 0; b < n; b++)
+{
+s1++;
+}
+for (b = n; b <= a - 1; ++b)
+{
+c = *s1;
+_putchar(c);
+s1++;
+}
+_putchar('\n');
+}
