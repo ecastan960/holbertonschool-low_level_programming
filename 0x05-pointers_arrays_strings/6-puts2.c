67c7df82f19f2c8afc1a13ca01e9eb9d6b07e39f
ecastan960 <ecastan1@eafit.edu.co>
2020-10-01 01:37:32 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * puts2 - This program will print using _putchar function
+ * @str: string
+ * Description: prints Holberton using the function
+ * Return: 0
+ *
+ */
+
+void puts2(char *str)
+{
+int a, b;
+char *s1, c;
+s1 = str;
+for (a = 0; str[a] != '\0'; ++a)
+{
+}
+for (b = 0; b < a - 1; ++b)
+{
+c = *s1;
+if (b % 2 == 0)
+{
+_putchar(c);
+}
+s1++;
+}
+_putchar('\n');
+}
