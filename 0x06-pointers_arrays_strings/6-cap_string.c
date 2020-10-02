5164ac1132f879eaaf1a353d41735920c2aeeda0
ecastan960 <ecastan1@eafit.edu.co>
2020-10-02 03:40:21 +0000

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * cap_string - This program will print using _putchar function
+ * Description: prints Holberton using the function
+ * Return: s
+ *
+ */
+
+char *cap_string(char *)
+{
+int d = 0, b, e = n;
+int *c;
+for (d = 0; d < n; d++)
+{
+c[d] = a[d];
+}
+for (b = 0; b < n; b++)
+{
+a[b] = c[e - 1];
+e = e - 1;
+}
+}
