2ad43cb8a39229750f325ac236a22bb2e6e29b57
ecastan960 <ecastan1@eafit.edu.co>
2020-10-06 04:09:55 +0000

    strc

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strstr - This program will print using _putchar function
+ * @haystack: array
+ * @needle: number
+ * Description: prints Holberton using the function
+ * Return: char
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+while (*s != c)
+{
+if (*s != 0)
+{
+s++;
+}
+else
+{
+s = 0;
+return (s);
+}
+}
+return (s);
+}
