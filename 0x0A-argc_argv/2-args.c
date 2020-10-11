6137d7b403bb214b7628b8ba755b0692007a1052
ecastan960 <ecastan1@eafit.edu.co>
2020-10-11 02:11:52 +0000

    args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,11 +2,11 @@
 
 /**
  * main - writes the character c to stdout
+ *
  * @argc: unused
  * @argv: name of program
  *
  * Return: i.
- * On error, -1 is returned, and errno is set appropriately.
  */
 
 int main(int __attribute__((unused)) argc, char *argv[])
@@ -16,7 +16,6 @@ while (argv[i])
 {
 printf("%s\n", argv[i]);
 i++;
-
 }
-return (i);
+return (0);
 }
