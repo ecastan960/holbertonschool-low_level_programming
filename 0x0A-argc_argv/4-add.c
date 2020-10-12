092910d390a4a2e440592919bd12e287e1aa9886
ecastan960 <ecastan1@eafit.edu.co>
2020-10-12 04:29:35 +0000

    temp

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,58 +6,16 @@
  * @argc: unused
  * @argv: name of program
  *
- * Return: i.
+ * Return: 0.
  */
 
 int main(int __attribute__((unused)) argc, char *argv[])
 {
-int n1 = 1, n2 = 0, a, b = 1, n = 0, c, d = 10;
-int num[2] = {1, 1};
-while (argv[n1])
+int i = 0;
+while (argv[i])
 {
-n1++;
+printf("%s\n", argv[i]);
+i++;
 }
-n1 = n1 - 1;
-if (n1 < 2)
-{
-printf("error\n");
-return (1);
-}
-else
-{
-for (a = 1; a <= 2; a++)
-{
-while (argv[a][n2])
-{
-n2++;
-}
-if (argv[a][0] == 45)
-{
-b = -1;
-n = 1;
-}
-else
-{
-b = 1;
-}
-for (c = n2 - 1; c >= n; --c)
-{
-if (c == n2 - 1)
-{
-num[a - 1] = argv[a][c] - 48;
-}
-else
-{
-num[a - 1] = ((argv[a][c] - 48) * (d)) + num[a - 1];
-d = d * 10;
-}
-}
-num[a - 1] *= num[a - 1] * b;
-n2 = 0;
-d = 10;
-}
-printf("%d\n", num[1] * num[0]);
 return (0);
 }
-}
-
