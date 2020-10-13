c8c3213040d5469856661ce6d79feeaadb7de5ec
ecastan960 <ecastan1@eafit.edu.co>
2020-10-13 23:16:28 +0000

    strtow

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * strtow - This program will print using _putchar function
+ * @str: size of array of char
+ * Description: prints Holberton using the function
+ * Return: s
+ *
+ */
+
+char **strtow(char *str)
+{
+unsigned int b, size;
+char *c;
+if (size <= 0)
+{
+return (0);
+}
+else
+{
+str = (char *)malloc(size * sizeof(char));
+if (str == 0)
+{
+return (0);
+}
+for (b = 0; b < size; b++)
+{
+a[b] = c;
+}
+return (a);
+}
+}
