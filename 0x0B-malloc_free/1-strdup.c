b1afd951126f1aee425961a9585ab66bc7701179
ecastan960 <ecastan1@eafit.edu.co>
2020-10-13 23:40:02 +0000

    dup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * create_array - This program will print using _putchar function
+ * _strdup - This program will print using _putchar function
  * @str: size of array of char
  * Description: prints Holberton using the function
  * Return: s
@@ -22,7 +22,7 @@ size++;
 }
 else
 {
-a = malloc((size + 1) *sizeof(char));
+a = malloc((size + 1) * sizeof(char));
 if (a == 0)
 {
 return (0);
