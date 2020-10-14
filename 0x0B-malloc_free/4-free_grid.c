342c535891f76e08280c7d42f665165b9e250440
ecastan960 <ecastan1@eafit.edu.co>
2020-10-14 04:07:22 +0000

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * free_grid - This program creates an array
+ * @grid: width of the array
+ * @height: height of the array
+ * Description: prints Holberton using the function
+ * Return: s
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+int a;
+for (a = 0; a < height; a++)
+{
+free(grid[a]);
+}
+}
