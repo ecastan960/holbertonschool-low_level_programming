7ec4e1019f20f27f04e2df058a78212b424eaf7e
ecastan960 <ecastan1@eafit.edu.co>
2020-10-14 04:38:58 +0000

    arg

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * argstostr - This program creates an array
+ * @ac: width of the array
+ * @av: height of the array
+ * Description: prints Holberton using the function
+ * Return: s
+ *
+ */
+
+char *argstostr(int ac, char **av)
+{
+int a = 0;
+char *s;
+
+s = malloc(ac * sizeof(char));
+for (a = 0; a < ac; a++)
+{
+s[a] = av[a];
+}
+return (s);
+}
