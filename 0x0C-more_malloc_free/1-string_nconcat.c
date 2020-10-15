3b4698fd79d6df7b05277329b4fbb0ae7ed6217b
ecastan960 <ecastan1@eafit.edu.co>
2020-10-15 21:50:51 +0000

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,34 +12,39 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int b, c = 0, size1 = 0, size2 = 0;
+unsigned int b, c = 0, si1 = 0, si2 = 0;
 char *a;
-while (s1[size1] != 0)
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+while (s1[si1] != 0)
 {
 size1++;
 }
-while (s2[size2] != 0)
+while (s2[si2] != 0)
 {
 size2++;
 }
-if (n < size2)
+if (n < si2)
 {
-size2 = n;
+si2 = n;
 }
-a = malloc((size1 + size2 + 1) * sizeof(char));
-if (a == NULL)
-{
-return (NULL);
-}
+
+a = malloc((si1 + si2 + 1) * sizeof(char));
 if (a == 0)
 {
 return (0);
 }
-for (b = 0; b < size1; b++)
+for (b = 0; b < si1; b++)
 {
 a[b] = s1[b];
 }
-for (c = 0; c <= size2; c++)
+for (c = 0; c <= si2; c++)
 {
 a[b + c] = s2[c];
 }
