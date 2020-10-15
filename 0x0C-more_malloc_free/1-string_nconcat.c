fbf758ee3903c6637979732bb70e45a65d3ffff7
ecastan960 <ecastan1@eafit.edu.co>
2020-10-15 21:33:51 +0000

    concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - Function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: size of memory allocate
+ * Description: prints Holberton using the function
+ * Return: s
+ *
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int b, c = 0, size1 = 0, size2 = 0;
+
+char *a;
+while (s1[size1] != 0)
+{
+size1++;
+}
+while (s2[size2] != 0)
+{
+size2++;
+}
+if (n < size2)
+{
+size2 = n;
+}
+
+a = malloc((size1 + size2 + 1) * sizeof(char));
+if (a == 0)
+{
+return (0);
+}
+for (b = 0; b < size1; b++)
+{
+a[b] = s1[b];
+}
+for (c = 0; c <= size2; c++)
+{
+a[b + c] = s2[c];
+}
+return (a);
+}
