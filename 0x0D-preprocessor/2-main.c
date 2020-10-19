699244725916467b21fe5fe1d8c3140129eeca86
ecastan960 <ecastan1@eafit.edu.co>
2020-10-19 14:43:57 +0000

    name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program 
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
