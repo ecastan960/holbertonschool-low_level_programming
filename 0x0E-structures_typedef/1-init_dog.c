929c697f30c61654a28b6dc0370ad3ab4f873236
ecastan960 <ecastan1@eafit.edu.co>
2020-10-20 02:57:50 +0000

    init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - writes the character c to stdout
+ *
+ * @d: function
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
