969885d1f2ed11dea0401423c08e5b45b309ff70
ecastan960 <ecastan1@eafit.edu.co>
2020-10-22 01:30:59 +0000

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+
+#include <stdlib.h>
+
+/**
+ * array_iterator - writes the character c to stdout
+ *
+ * @array: Array to use
+ * @size: Size of array
+ * @action: Point function
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+if (array != NULL && action != NULL)
+{
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
+}
