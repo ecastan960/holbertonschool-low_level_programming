08520c2e1538ea943933a1621be74886052cb77b
ecastan960 <ecastan1@eafit.edu.co>
2020-10-25 13:27:53 +0000

    print_numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - function that sums up numbers.
+ *
+ * @n: Size of assigments
+ * @separator: character separator to use
+ *
+ * Description: returns the sum of all the parameters.
+ */
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+const char *s;
+va_list pa;
+s = separator;
+if (n != 0)
+{
+va_start(pa, n);
+for (i = 0; i < n ; i++)
+{
+if (i < n - 1)
+{
+if (s != 0)
+{
+printf("%d%c ", va_arg(pa, int), *separator);
+}
+else
+{
+printf("%d ", va_arg(pa, int));
+}
+}
+if (i == n - 1)
+printf("%d\n", va_arg(pa, int));
+}
+va_end(pa);
+}
+}
