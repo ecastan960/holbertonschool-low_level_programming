cf8f99d55173af8862861b446ff22abdf29d9052
ecastan960 <ecastan1@eafit.edu.co>
2020-10-29 01:25:26 +0000

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,30 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
-/**
- * form - function that selects the formating to use.
- *
- * @d: format letter
- *
- * Description: Functions that selects format base on a letter.
- * Return: String with the appropiate format.
- */
 
-char *form(int d)
-{
-switch (d)
-{
-case 99:
-return ("%c");
-case 105:
-return ("%d");
-case 102:
-return ("%f");
-case 115:
-return ("%s");
-}
-return (0);
-}
 
 /**
  * print_all - function that prints stuff.
@@ -36,32 +13,43 @@ return (0);
 
 void print_all(const char * const format, ...)
 {
-int i = 0, c = 0, n = 0;
-char *str;
-va_list print;
-str = (char *)malloc(sizeof(char));
-while (format[n])
-{
-if (form(format[n]) != 0)
-{
-str[c] = format[n];
-str = (char *)realloc(str, (c + 2) * sizeof(char));
-c = c + 1;
-}
-n++;
-}
+	int i = 0, c = 0, n = 0, sep = 0;
+	char *str;
+	va_list print;
 
-va_start(print, format);
-while (i < c)
-{
-char *s = form(str[i]);
-char *d = va_arg(print, int*);
-printf(s, d);
-if (i < c - 1)
-{
-printf(", ");
-}
-i++;
-}
-printf("\n");
+	while (format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c",  va_arg(print, int*));
+			i++;
+			sep = 1;
+			break;
+		case 'i':
+			printf("%d",  va_arg(print, int*));
+			i++;
+			sep = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(print, int*));
+			i++;
+			sep = 1;
+			break;
+		case 's':
+			printf("%s", va_arg(print, int));
+			i++;
+			sep = 1;
+			break;
+		default:
+			i++;
+			sep = 0;
+			break;
+		}
+		if (i < n && sep == 1)
+		{
+			printf(", ");
+		}
+
+	}
 }
