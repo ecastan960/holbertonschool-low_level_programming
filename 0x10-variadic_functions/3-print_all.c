169dfd6edca7ef1bf699975743d4a51587ac7cbc
ecastan960 <ecastan1@eafit.edu.co>
2020-10-29 01:41:18 +0000

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
-
 
 /**
  * print_all - function that prints stuff.
@@ -13,43 +11,41 @@
 
 void print_all(const char * const format, ...)
 {
-	int i = 0, c = 0, n = 0, sep = 0;
-	char *str;
+	int i = 0, n = 0, sep = 0;
 	va_list print;
 
+	while (format[n])
+		n++;
+	va_start(print, format);
 	while (format[i])
 	{
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c",  va_arg(print, int*));
-			i++;
-			sep = 1;
+			printf("%c",  va_arg(print, int));
+			i++, sep = 1;
 			break;
 		case 'i':
-			printf("%d",  va_arg(print, int*));
-			i++;
-			sep = 1;
+			printf("%d",  va_arg(print, int));
+			i++, sep = 1;
 			break;
 		case 'f':
-			printf("%f", va_arg(print, int*));
-			i++;
-			sep = 1;
+			printf("%f", va_arg(print, double));
+			i++, sep = 1;
 			break;
 		case 's':
-			printf("%s", va_arg(print, int));
-			i++;
-			sep = 1;
+			printf("%s", va_arg(print, char*));
+			i++, sep = 1;
 			break;
 		default:
-			i++;
-			sep = 0;
+			i++, sep = 0;
 			break;
 		}
 		if (i < n && sep == 1)
 		{
 			printf(", ");
 		}
-
 	}
+	va_end(print);
+	printf("\n");
 }
