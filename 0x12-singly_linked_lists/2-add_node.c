91e1c1e5efdedae50579348285c9bab931ec9534
ecastan960 <ecastan1@eafit.edu.co>
2020-10-31 01:47:05 +0000

    node head

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_node - prints a string as integer
+ *
+ * @head: head
+ * @str: string
+ *
+ * Return: contador_fi
+ */
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
