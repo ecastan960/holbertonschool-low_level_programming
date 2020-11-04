945eb2aa8e7d887e096872f7d1d62b32a5387077
ecastan960 <ecastan1@eafit.edu.co>
2020-11-04 22:59:23 +0000

    free2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * free_listint2- function that frees a list
+ *
+ * @head: head
+ *
+ * Return: contador_fi
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head != NULL && (*head)->next != NULL)
+	{
+		temp = *head;
+		(*head) = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
