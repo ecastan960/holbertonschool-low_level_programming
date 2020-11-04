144c5a3c37d1771769c9e080f44100efc5141265
ecastan960 <ecastan1@eafit.edu.co>
2020-11-04 16:49:51 +0000

    test empty

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+
+
+/**
+ * free_list- prints a string as integer
+ *
+ * @head: head
+ *
+ * Return: contador_fi
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
