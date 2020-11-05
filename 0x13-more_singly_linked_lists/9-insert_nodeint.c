62ed5239ba3083f3be87d43573bfeda6618659a3
ecastan960 <ecastan1@eafit.edu.co>
2020-11-05 06:21:40 +0000

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (temp != NULL)
 	{
-		if (node == idx - 1)
+		if (node <= idx)
 		{
 			new->next = temp->next;
 			temp->next = new;
