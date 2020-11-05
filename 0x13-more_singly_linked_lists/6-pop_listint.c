e9c0e64f7acc857bb38eae5babda357572449875
ecastan960 <ecastan1@eafit.edu.co>
2020-11-05 00:14:27 +0000

    pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,11 +13,20 @@
 
 int pop_listint(listint_t **head)
 {
+	int num;
+	listint_t *temp;
 
 	if (*head == NULL)
+	{
 		return (0);
+	}
+	else
+	{
+		num = (*head)->n;
+		temp = *head;
+		(*head) = (*head)->next;
+		free(temp);
+	}
 
-	(*head) = (*head)->next;
-
-	return (0);
+	return (num);
 }
