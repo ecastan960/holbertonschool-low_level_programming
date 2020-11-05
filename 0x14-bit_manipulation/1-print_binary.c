28127845213a818bed88b64f4cd418eb219beabd
ecastan960 <ecastan1@eafit.edu.co>
2020-11-05 23:26:08 +0000

    printb

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * binary- function that finds int equivalent
+ *
+ * @n: number of spaces
+ *
+ * Return: Nth node
+ */
+unsigned long int binary(int n)
+{
+	int i;
+	unsigned long int  x = 1;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		x = x * 2;
+	}
+	return (x);
+}
+
+/**
+ * print_binary- function that inserts a new node
+ *
+ * @n: string containing the binary number
+ *
+ * Return: Nth node
+ */
+
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int x = 1, s = 1, i, num = n;
+
+
+	while (n > x)
+	{
+		x = x * 2;
+		s++;
+	}
+	for (i = 0; i < s; i++)
+	{
+		if (n >= binary(s - i))
+		{
+			printf("1");
+			num = num - binary(s - i);
+		}
+		else
+		{
+			printf("0");
+		}
+	}
+}
