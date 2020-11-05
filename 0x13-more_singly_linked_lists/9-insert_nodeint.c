90104d7e549798b896f56a461d1c6ce0b09ddb9e
ecastan960 <ecastan1@eafit.edu.co>
2020-11-05 06:15:45 +0000

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,28 +16,32 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int node = 0;
-	listint_t **temp = (listint_t **) malloc(sizeof(listint_t));
 
-	*temp = *head;
+	listint_t (*temp) = (*head);
+	listint_t *new = (listint_t *) malloc(sizeof(listint_t));
+
+
+	new->next=NULL;
+	new->n=n;
 
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-	while (*temp != NULL)
+	while (temp != NULL)
 	{
 		if (node == idx - 1)
 		{
-			(*temp)->n = n;
-			(*temp) = (*temp)->next;
+			new->next =temp->next;
+			temp->next = new;
 			node++;
 		}
 		else
 		{
-			(*temp) = (*temp)->next;
-
+			temp = temp->next;
 			node++;
 		}
+		
 	}
-	return (*temp);
+	return (temp);
 }
