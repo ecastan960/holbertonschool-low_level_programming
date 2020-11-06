f108433504069796c20ff390f54f9e45010c8a99
ecastan960 <ecastan1@eafit.edu.co>
2020-11-06 01:53:28 +0000

    holberton

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -32,27 +32,37 @@ unsigned long int binary(int n)
 void print_binary(unsigned long int n)
 {
 	unsigned long int x = 1, s = 1, i, num = n;
-
+	int r = 0;
 
 	while (n > x)
 	{
 		x = x * 2;
 		s++;
+		if (n == x)
+		{
+			for (i = 0; i < s; i++)
+			{
+				if (i == 0)
+					_putchar('1');
+				else
+					_putchar('0');
+			}
+			r = 1;
+		}
 	}
 	if (n > 1)
+		s--;
+	if (r == 0)
 	{
-		s = s - 1;
-	}
-	for (i = 0; i < s; i++)
-	{
-		if (n >= binary(s - i))
+		for (i = 0; i < s; i++)
 		{
-			_putchar('1');
-			num = num - binary(s - i);
-		}
-		else
-		{
-			_putchar('0');
+			if (num >= binary(s - i))
+			{
+				_putchar('1');
+				num = num - binary(s - i);
+			}
+			else
+				_putchar('0');
 		}
 	}
 }
