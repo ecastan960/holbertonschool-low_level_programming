b84faae972e04ffb0a170345b905dc1dedd289ac
ecastan960 <ecastan1@eafit.edu.co>
2020-11-06 03:26:28 +0000

    set

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * binary- function that finds int equivalent
+ *
+ * @n: number of spaces
+ *
+ * Return: Nth node
+ */
+unsigned long int binary(int n)
+{
+	int i;
+	unsigned long int  x = 1;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		x = x * 2;
+	}
+	return (x);
+}
+
+/**
+ * set_bit- function that inserts a new node
+ *
+ * @n: string containing the binary number
+ * @index: string containing the binary number
+ *
+ * Return: bit
+ */
+
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int x = 1, result;
+	unsigned int count = 0;
+
+	if (n == NULL)
+		return (-1);
+	while (count < index)
+	{
+		x = x * 2;
+		count++;
+	}
+	if (x < *n)
+	{
+		result = x + *n;
+		n = &result;
+		return (1);
+	}
+	else
+	{
+		result = x;
+		n = &result;
+		return (1);
+	}
+	return (1);
+}
