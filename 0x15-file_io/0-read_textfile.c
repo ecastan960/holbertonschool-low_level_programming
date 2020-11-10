00c1f527b5bfb55d7495e082411af055628733ad
ecastan960 <ecastan1@eafit.edu.co>
2020-11-10 14:00:25 +0000

    header

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,22 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * read_textfile - check the code for Holberton School students.
