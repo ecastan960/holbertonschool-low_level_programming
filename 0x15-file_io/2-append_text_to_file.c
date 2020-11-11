e0e58690df50d986d1a3c9bfbe44f360c8584c2b
ecastan960 <ecastan1@eafit.edu.co>
2020-11-11 02:30:13 +0000

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <elf.h>
+
+
+/**
+ * create_file - function that creates a file with text
+ *
+ * @filename: Pointer to filaname
+ * @text_content: content of the file
+ *
+ * Return: Always 0.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+	int fname, n = 0, a;
+
+	while (text_content[n])
+	{
+		n++;
+	}
+
+	if (filename)
+		return (-1);
+
+	fname = open(filename, O_RDWR | O_APPEND, 0600);
+
+	if (fname == -1)
+	{
+		return (-1);
+	}
+
+	a = write(fname, text_content, 0);
+	if (a == -1)
+		return (-1);
+	close(fname);
+	return (1);
+
+}
