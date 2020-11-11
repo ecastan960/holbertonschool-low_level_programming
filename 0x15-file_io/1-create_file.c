85e9932c85a94f1d65afefa451eecdfd6c34d5ec
ecastan960 <ecastan1@eafit.edu.co>
2020-11-11 00:55:27 +0000

    create

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * create_file - function that creates a file with text
+ *
+ * @filename: Pointer to filaname
+ * @text_content: content of the file
+ *
+ * Return: Always 0.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+
+	int fname;
+
+	if (filename)
+		return (-1);
+
+	fname = open(filename, O_CREAT | O_WRONLY, 0600);
+
+	if (fname == -1)
+	{
+		return (-1);
+	}
+	write(fname, text_content, 10);
+
+	close(fname);
+	return (1);
+
+}
