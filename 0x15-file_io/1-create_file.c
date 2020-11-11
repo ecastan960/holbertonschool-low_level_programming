597775dca3643b127ceeaca4793598a054578427
ecastan960 <ecastan1@eafit.edu.co>
2020-11-11 01:56:00 +0000

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,6 +5,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <elf.h>
+
 
 /**
  * create_file - function that creates a file with text
@@ -18,19 +20,25 @@
 int create_file(const char *filename, char *text_content)
 {
 
-	int fname;
+	int fname, n = 0, a;
+	while (text_content[n])
+	{
+		n++;
+	}
 
 	if (filename)
 		return (-1);
 
-	fname = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fname = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fname == -1)
 	{
 		return (-1);
 	}
-	write(fname, text_content, 10);
 
+	a = write(fname, text_content, 0);
+	if (a == -1)
+		return (-1);
 	close(fname);
 	return (1);
 
