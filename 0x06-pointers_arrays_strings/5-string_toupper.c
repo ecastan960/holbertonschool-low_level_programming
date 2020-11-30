fe1561598736b18c7a57422938b717d34ec229e9
ecastan960 <ecastan1@eafit.edu.co>
2020-11-30 20:50:49 +0000

    05|Betty fix using NULL try

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * string_toupper - This program will print using _putchar function
  * Description: prints Holberton using the function
@@ -10,9 +10,9 @@
 
 char *string_toupper(char * s)
 {
-	int  i = 0, temp, b = 1;
+	int  i = 0;
 
-	while (s[i] != NULL)
+	while (s[i] != '\0')
 	{
 		if (s[i] > 96 && s[i] < 123)
 		{
