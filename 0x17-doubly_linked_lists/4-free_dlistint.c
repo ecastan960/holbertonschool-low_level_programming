65abb22b126a6230aeb2bd354d203a653f4c9161
ecastan960 <ecastan1@eafit.edu.co>
2020-12-13 19:22:09 -0500

    05|First test

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_dlistint- free memory of nodes of a BL
+ * @head: head pointer of list
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+
+	if (head == NULL)
+	{
+		exit(1);
+	}
+	while (head != NULL)
+	{
+		head = head->next;
+		free(temp);
+		temp = head;
+	}
+}
