b93cd41a6733eaae1172be628077cc19e934969a
ecastan960 <ecastan1@eafit.edu.co>
2020-12-14 23:56:18 -0500

    13|first test

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index- delete node at index
+ * @head: head pointer of list
+ * @index: index of new node
+ *
+ * Return: New head pointer
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+	unsigned int x = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	while (temp != NULL)
+	{
+		if (x == index && temp->next == NULL && temp->prev == NULL)
+		{
+			temp = NULL;
+		}
+		if (x == index)
+		{
+			temp = temp->prev;
+			temp->next = temp->next->next;
+			temp->prev = temp->prev->prev;
+		}
+	}
+	return (1);
+}
