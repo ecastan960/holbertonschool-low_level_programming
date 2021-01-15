69a94ac3dffc11064de7e5b7ddd7157f69d81896
ecastan960 <ecastan1@eafit.edu.co>
2021-01-15 15:39:29 -0500

    08|add case of same key with othe value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,8 +24,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (key == NULL)
 		return (0);
-	if (key[0] == '\0')
-		return (0);
 	if (ht->array == NULL)
 		return (0);
 	if (ht->size == 0)
