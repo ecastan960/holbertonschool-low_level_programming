9796eca472297dacdeda7d001bce9b1a60a2e373
ecastan960 <ecastan1@eafit.edu.co>
2021-01-15 15:09:58 -0500

    06|add case of same key with othe value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,10 +15,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	char *copy_v = malloc(sizeof(char));
 	char *copy_key = malloc(sizeof(char));
+	hash_node_t *tmp;
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
 	if (new_node == NULL)
 		return (0);
-
 	if (ht == NULL)
 		return (0);
 	if (key == NULL)
@@ -36,7 +37,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((unsigned char *)key, ht->size);
 	strcpy(copy_v, value);
 	strcpy(copy_key, key);
-
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = copy_v;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
 	new_node->key = copy_key;
 	new_node->value = copy_v;
 	new_node->next = ht->array[index];
