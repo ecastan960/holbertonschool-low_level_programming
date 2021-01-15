e22ea0b5476913065888557a8f218b5a8cf831a6
ecastan960 <ecastan1@eafit.edu.co>
2021-01-15 14:59:18 -0500

    04|first test

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#include <string.h>
+/**
+ * hash_table_set -  Function inserts a key and a value to the
+ * hash table.
+ * @ht: Node key
+ * @key: size of the array
+ * @value: Value in node of key
+ *
+ * Return: pointer to the newly created hash table.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	char *copy_v = malloc(sizeof(char));
+	char *copy_key = malloc(sizeof(char));
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
+	if (ht == NULL)
+		return (0);
+	if (key == NULL)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	strcpy(copy_v, value);
+	strcpy(copy_key, key);
+
+	new_node->key = copy_key;
+	new_node->value = copy_v;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
