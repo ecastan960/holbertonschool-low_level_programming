6115572c52b970b3fdf1efb31e34bf842ec858ae
ecastan960 <ecastan1@eafit.edu.co>
2021-02-21 17:12:30 -0500

    25|fix to perimeter value

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,6 +3,78 @@
     """
 
 
+def check_around(x, y, grid):
+    """[summary]
+
+    Args:
+        x ([type]): [description]
+        y ([type]): [description]
+        grid ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    per = 0
+    if y == 0 and x == 0:
+        if grid[0][1] == 0:
+            per += 1
+        if grid[1][0] == 0:
+            per += 1
+        return (per + 2)
+    elif y == len(grid[x]) - 1 and x == 0:
+        if grid[x][y - 1] == 0:
+            per += 1
+        if grid[x + 1][y] == 0:
+            per += 1
+        return (per + 2)
+    elif y == len(grid[x]) - 1 and x == len(grid) - 1:
+        if grid[x - 1][y] == 0:
+            per += 1
+        if grid[x][y - 1] == 0:
+            per += 1
+        return (per + 2)
+    elif y == 0 and x == len(grid) - 1:
+        if grid[x][y + 1] == 0:
+            per += 1
+        if grid[x - 1][y] == 0:
+            per += 1
+        return (per + 2)
+    elif y == 0 and x != len(grid) - 1:
+        if grid[x][1] == 0:
+            per += 1
+        if grid[x + 1][0] == 0:
+            per += 1
+        if grid[x - 1][0] == 0:
+            per += 1
+        return (per + 1)
+    elif x == 0 and y != len(grid[x]) - 1:
+        if grid[0][1] == 0:
+            per += 1
+        if grid[0][y + 1] == 0:
+            per += 1
+        if grid[0][y - 1] == 0:
+            per += 1
+        return (per + 1)
+    elif x == len(grid) - 1 and y != len(grid[x]) - 1:
+        if grid[x][y - 1] == 0:
+            per += 1
+        if grid[x][y + 1] == 0:
+            per += 1
+        if grid[x - 1][y] == 0:
+            per += 1
+        return (per + 1)
+    else:
+        if grid[x][y + 1] == 0:
+            per += 1
+        if grid[x][y - 1] == 0:
+            per += 1
+        if grid[x + 1][y] == 0:
+            per += 1
+        if grid[x - 1][y] == 0:
+            per += 1
+        return (per)
+
+
 def island_perimeter(grid):
     """[summary]
 
@@ -14,21 +86,11 @@ def island_perimeter(grid):
     """
     cx = 0
     cy = 0
+    per = 0
     previous_x = 0
     previous_y = 0
     for x in range(len(grid)):
         for y in range(len(grid[x])):
-            if grid[x][y] == 1 and cx == 0:
-                cx += 1
-                cy += 1
-                previous_x = x
-                previous_y = y
-            elif grid[x][y] == 1 and previous_x == x:
-                cx += 1
-                previous_x = x
-                previous_y = y
-            elif grid[x][y] == 1 and previous_y == y:
-                cy += 1
-                previous_x = x
-                previous_y = y
-    return ((cx * 2) + (cy * 2))
+            if grid[x][y] == 1:
+                per = per + check_around(x, y, grid)
+    return (per)
