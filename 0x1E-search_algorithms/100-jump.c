d9be55f49ded1db432dcdfba420637730a92e21b
ecastan960 <ecastan1@eafit.edu.co>
2021-04-21 11:22:43 -0500

    00|First test

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,58 @@
+#include "search_algos.h"
+#include <math.h>
+/**
+ * l_search - linear search algorithm
+ *
+ * @array: pointer to the first element of the array to search
+ * @left: First index to search
+ * @right: Last index to serach
+ * @value: Value to be found inside the array
+ * Return: Index location of value inside array or -1
+ * if its not found
+ */
+size_t l_search(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
+
+	for (i = left; i <= right; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
+
+/**
+ * jump_search - binary search algorith
+ *
+ * @array: pointer to the first element of the array to search
+ * @size: Number of elements inside the array
+ * @value: Value to be found inside the array
+ * Return: Index location of value inside array or -1
+ * if its not found
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t j = sqrt(size - 1), index = 0;
+
+	while (index <= size - 1)
+	{
+		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+			return (index);
+		else if (array[index + j] >= value || index + 1 == size)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n", index, index + j);
+			if (index + 1 == size)
+				index = l_search(array, index, size - 1, value);
+			else
+				index = l_search(array, index, index + j, value);
+			return (index);
+		}
+		else
+			index = index + j;
+	}
+	return (-1);
+}
